981b8a314b3a666b1cb760a56241c2c2e985dc83
hitohiro-sh <81672758+hitohiro-sh@users.noreply.github.com>
2021-12-31 22:35:56 +0900

    Add files via upload

---
+++ S_exp_prettyprint.py
@@ -0,0 +1,32 @@
+import sys
+import io
+
+def main(argv):
+    with open(argv[1]) as input:
+        stack = []
+        out = io.StringIO()
+        prev_c = ''
+        for line in input.readlines():
+            for c in line:
+                if c == '(':
+                    out.write('\n')
+                    out.write(' ' * len(stack))
+                    out.write(c)
+                    stack.append(c)
+                    prev_c = c
+                elif c == ')':
+                    out.write(c)
+                    stack.pop()
+                    prev_c = c
+                elif c.isspace():
+                    if prev_c == '(' or prev_c == ')' or prev_c.isspace():
+                        continue
+                    out.write(' ')
+                else:
+                    out.write(c)
+                    prev_c = c
+        print(out.getvalue())
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
