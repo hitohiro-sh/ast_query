d3fcccc4937f12d7ea78b62ab417263ab1422241
Hitohiro Shiozaki <h.shiozaki.it.just@gmail.com>
2022-01-05 17:17:58 +0900

    remove comment out.

--- fordirs.py
+++ fordirs.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import subprocess
 from typing import Pattern
 
-def main(argv):
+def main(argv : list[str]):
     print(argv)
     path = Path(argv[1])
     pattern = argv[2]
@@ -12,7 +12,6 @@ def main(argv):
     # '<cmd> {}'        => replace {} to file
     # '<cmd1>' -p '<cmd2>' => pipe <cmd1> to <cmd2>
     cmd_templates = argv[3:]
-    #print(cmd_templates)
     if path.is_dir():
         for file in sorted(path.glob(pattern)):
             print(str(file))
@@ -40,18 +39,6 @@ def main(argv):
 
                     ret = subprocess.run(cmd, input=input_arg, stdout=stdout_arg)
                     from_pipe_flag = False
-                
-            #cmd_template1 = cmd_templates[0]
-            #cmd_template2 = cmd_templates[1]
-
-            #cmd = cmd_template1.format(str(file)).split(' ')
-            #print('cmd:', cmd)
-            #ret = subprocess.run(cmd, stdout=subprocess.PIPE)
-            #print(ret.stdout)
-            #cmd = cmd_template2.format(str(file)).split(' ')
-            #print(cmd)
-            #ret = subprocess.run(cmd, input=ret.stdout)
-
 
 if __name__ == '__main__':
     main(sys.argv)
