029eed7e41e7bd4957fbcd98e1582bbd940a640d
Hitohiro Shiozaki <h.shiozaki.it.just@gmail.com>
2022-01-17 22:09:14 +0900

    yield style.

--- python_parser/python/search.py
+++ python_parser/python/search.py
@@ -2,7 +2,24 @@ import sys
 
 import re
 
-def main(argv):
+
+
+def iter(comp_func, input):
+    for line in input.readlines():
+        #if re.search(pattern, line):
+        if comp_func(line):
+            yield line
+            #print(line)
+
+'''
+argv:
+  0 prog
+  1 option:
+      -p
+      -l
+      -s
+'''
+def config(argv):
     if len(argv) > 1 and argv[1] == '-p':
         pattern = argv[2]
         pattern = re.compile(pattern)
@@ -13,12 +30,14 @@ def main(argv):
         
         comp_func = lambda line: pattern == line
 
-    output = sys.stdin
+    return comp_func
 
-    for line in output.readlines():
-        #if re.search(pattern, line):
-        if comp_func(line):
-            print(line)
+
+def main(argv, input = sys.stdin, output = sys.stdout):
+    comp_func = config(argv)
+
+    for line in iter(comp_func, input):
+        print(line, file=output)
 
 if __name__ == '__main__':
     main(sys.argv)
